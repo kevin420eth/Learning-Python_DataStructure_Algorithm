08388323e4da040a80cb270a09bb071d7672469b
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-09 20:23:03 +0800

    Modified dynamic programming

--- algorithm/dynamic_programming.py
+++ algorithm/dynamic_programming.py
@@ -1,6 +1,6 @@
-#Calculating the Fibonacci series
+# Calculating the Fibonacci series
 
-#overlapping
+# With recursive approach
 def calFibonacci_array_1(length):
     fibonacci_array = []
     for _ in range(length):
@@ -14,9 +14,8 @@ def calFibonacci_number(index):
     else:
         return calFibonacci_number(index-1) + calFibonacci_number(index-2)
 
-#----------------------How dynamic programming works----------------------#
 
-#non-overlapping
+# With dynamic programming approach
 def calFibonacci_array_2(length):
     fibonacci_array = []
     index=0
@@ -30,7 +29,12 @@ def calFibonacci_array_2(length):
             index+=1
     return fibonacci_array
 
-print(calFibonacci_array_1(40))
+print(calFibonacci_array_2(40))
 
-# You can try calFibonacci_array_1 with some big input data, in my case, when length argument is bigger than 30,
-# the run time becomes much more longer (My GPU is GTX-1060)
+
+# ----------------------------Analysis----------------------------
+# You can try calFibonacci_array_1 with some big input data, in my case, when length argument is bigger 
+# than 30, the run time becomes much more longer (My GPU is GTX-1060)
+# 
+# The time complexity for the recursive approach is O(2^n)
+# The time complexity for the dynamic programming is O(n)
